4d8134ac9429dc38fb416b0f58eaf9434d9feb6e
nouseforaname <none@fuckoff.com>
2024-11-13 23:18:08 +0100

    simplify brightness code
    the example it was taken from was unnecessary complicated. This seems to do the job
    just as well

--- components/internal/display.cpp
+++ components/internal/display.cpp
@@ -6,30 +6,12 @@
 TFT_eSPI tft = TFT_eSPI();
 void setBrightness(uint8_t value)
 {
-  static uint8_t steps = 16;
-  static uint8_t _brightness = 0;
-
-  if (_brightness == value) {
-      return;
+  if (value == 0){
+    return;
   }
-
-  if (value > 16) {
-      value = 16;
-  }
-  if (value == 0) {
-      digitalWrite(BK_LIGHT_PIN, 0);
-      _brightness = 0;
-      return;
-  }
-  if (_brightness == 0) {
-      digitalWrite(BK_LIGHT_PIN, 1);
-      _brightness = steps;
-      delayMicroseconds(30);
-  }
-  int from = steps - _brightness;
-  int to = steps - value;
-  int num = (steps + to - from) % steps;
-  for (int i = 0; i < num; i++) {
+  digitalWrite(BK_LIGHT_PIN, 1);
+  uint8_t steps = 16 - value;
+  for (int i = 0; i < steps; i++) {
       digitalWrite(BK_LIGHT_PIN, 0);
       digitalWrite(BK_LIGHT_PIN, 1);
   }
@@ -45,11 +27,7 @@ void init_display() {
   digitalWrite(PWR_EN_PIN, HIGH);
 
   tft.init();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-  // Set backlight level, range 0 ~ 16
-  uint8_t brightness = 16;
-  setBrightness(brightness);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_GREEN);
+  setBrightness(16);
+  tft.fillScreen(BACKGROUND_COLOR);
+  tft.setTextColor(FONT_COLOR,BACKGROUND_COLOR);
 }
