7794363e7e8dc38560e87b45e7e4adec7da6415f
nouseforaname <none@fuckoff.com>
2024-11-13 23:18:08 +0100

    amend display

--- main/gamba-display.cpp
+++ main/gamba-display.cpp
@@ -6,16 +6,8 @@
 #include "freertos/task.h"
 #include <esp_log.h>
 
-extern "C" void app_main() {
-  // Set up the application
-  setup();
-  for (;;) {
-    loop();
-    yield();
-  }
-}
-
-char log_tag[] = "main";
+char main_tag[] = "main";
+char sensor_tag[] = "sensor";
 static char s[14]="";
 
 void readSensors(void* pvParameters){
@@ -29,11 +21,6 @@ void readSensors(void* pvParameters){
 
 void setup()
 {
-  printf("begin setup\n");
-  pinMode(PWR_EN_PIN, OUTPUT);
-  pinMode(38, OUTPUT);  // Code Crashed without setting this. not totally sure why..
-  digitalWrite(PWR_EN_PIN, HIGH);
-
   water_init();
   init_display();
   init_input();
