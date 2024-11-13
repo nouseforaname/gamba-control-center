85b6e3ecab3e60086252a91c6d8cd4e9a78e487f
nouseforaname <none@fuckoff.com>
2024-11-13 23:18:08 +0100

    ignore built crap

--- main/gamba-display.cpp
+++ main/gamba-display.cpp
@@ -10,36 +10,38 @@ char main_tag[] = "main";
 char sensor_tag[] = "sensor";
 static char s[14]="";
 
-void readSensors(void* pvParameters){
+void sensorLoop(void* pvParameters){
   for (;;) {
-
     sprintf(s, "Wasser: %03d", getWaterLevel());
-    delay(WAIT);
-
+    vTaskDelay(100); // so other crap can get to RUN (IDLE TASK) and it can reset the watchdog 
   }
 }
-
+void renderLoop(){
+  for (;;) {
+    getTFT()->drawString(s,0,0,4);
+    handleAndDrawInput();
+    vTaskDelay(100); // so other crap can get to RUN (IDLE TASK) and it can reset the watchdog 
+  }
+}
 void setup()
 {
   water_init();
   init_display();
   init_input();
-  ESP_LOGI(log_tag,"app setup done");
+
+  ESP_LOGI(main_tag,"app setup done");
   xTaskCreate(
-    readSensors,     // Function to implement the task
-    "loop2",   // Name of the task
-    1000,      // Stack size in bytes
+    sensorLoop,     // Function to implement the task
+    "sensors",   // Name of the task
+    10000,      // Stack size in bytes
     NULL,      // Task input parameter
-    0,         // Priority of the task
+    1,         // Priority of the task
     NULL      // Task handle.
   );
 }
 
-
-void loop()
-{
-  handleAndDrawInput(tft);
-
-  delay(WAIT);
-
+extern "C" void app_main() {
+  // Set up the application
+  setup();
+  renderLoop();
 }
